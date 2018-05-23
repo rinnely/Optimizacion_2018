d7675bc4122f255832a11971f5bd96cdc1827b4a
rinnely <nohelysfm@gmail.com>
2018-05-23 14:28:14 -0500

    Update distance.cpp

--- src/distance.cpp
+++ src/distance.cpp
@@ -1,8 +1,11 @@
-	#include "DisplayImage.cpp"
-  Display_image di;
+#include "DisplayImage.cpp"
+#include <string>
 
-	// Import biomass and friction
-  str map_biomass = "nombre_de_la_archivo"
-	clock_t begin = clock();
-	float** biomass = di.tiff_to_matrix_gdal(map_biomass, true);
-	float** friction = di.tiff_to_matrix_gdal(map_friction, false);
+using namespace std;
+Display_image di;
+
+// Import biomass and friction
+string map_biomass = "biomass.tiff";
+string map_friction = "friction.tiff";
+float** biomass = di.tiff_to_matrix_gdal(map_biomass, true);
+float** friction = di.tiff_to_matrix_gdal(map_friction, false);
