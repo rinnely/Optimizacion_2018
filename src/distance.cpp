82915baee47c055a21d649740becb0dfc37f167e
JorgeAlanisN <36005129+JorgeAlanisN@users.noreply.github.com>
2018-05-23 15:40:58 -0500

    Update distance.cpp

--- src/distance.cpp
+++ src/distance.cpp
@@ -9,3 +9,17 @@ string map_biomass = "biomass.tiff";
 string map_friction = "friction.tiff";
 float** biomass = di.tiff_to_matrix_gdal(map_biomass, true);
 float** friction = di.tiff_to_matrix_gdal(map_friction, false);
+
+void distanceMatrix(float** biomass, float** friction, int X, int Y){
+  float** cost;
+  vecinos(X,Y,cost,biomass,friction);
+
+  
+}
+
+float** getDistance(float** biomass, float** friction, int* centroidesX, int* centroidesY, int N){
+  #pragma paralel for
+    for(int i = 0; i < N; ++i){
+      distanceMatrix(biomass,friction,centroidesX[i],centroidesY[i]);
+    }
+}
